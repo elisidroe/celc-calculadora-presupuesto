3bb4cffbf90a762f0bc9a374aea30ef18906fcc1
elisidore <celc04lerdo@gmail.com>
2024-07-24 10:01:10 -0600

    second commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ class Presupuesto {
         this.restante = this.presupuesto - gastado;
     }
 
-    EliminarGasto(){
+    eliminarGasto(){
         this.gastos = this.gastos.filter (gasto => gasto.id !== id);
         this.calcularRestante();
     }
@@ -46,7 +46,73 @@ class Presupuesto {
         //crear div
         const divMensaje = document.createElement('div');
         divMensaje.classList.add('text-align: center', 'blackground-color: #007bff');
+
+        if(tipo === 'error'){
+            divMensaje.classList.add('padding: 15 ppx', 'margin-bottom: 20px', 'border: 1px solid transparent', 'border-radius: 3px');
+        } else{
+            divMensaje.classList.add('background-color: #dff0d8', 'border-color: #d6e9c6', 'color: #3c763d');
+        } 
+        
+        // mensaje de error
+
+        divMensaje.textContent = mensaje;
+
+        //insertar HTML
+
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        //Quitar el mensaje a los 3 segundos
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000)
     }
+
+    mostrarGastos(gastos){
+        this.limpiarHTML(); //Esta funcion elimina el HTML previo
+
+        // iterar sobre los gastos
+
+        gastos.forEach(gastos => {
+            const {cantidad, nombre, id } = gasto;
+
+            //Crear las li
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = '';
+            nuevoGasto.dataset.id = id;
+
+            //agregar el HTML del gasto
+            nuevoGasto.innerHTML = `${nombre} <span> $ ${cantidad} </span>`;
+            
+            //boton para borrar el gasto
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'borrar-gasto');
+
+            btnBorrar.innerHTML = 'Borrar'
+
+            // funcion para borrar el gasto
+            btnBorrar.onclick = () =>{
+                eliminarGasto(id);
+            }
+
+            nuevoGasto.appendChild(btnBorrar);
+
+            gastolistado.appendChild(nuevoGasto);
+        });
+    }
+
+            limpiarHTML(){
+                while(gastolistado.firstChild){
+                    gastolistado.removeChild(gastolistado.firstChild);
+                }
+            }
+            actualizarRestante(restante){
+                document.querySelector('restante').textContent = restante;
+            }
+            comprobarPresupuesto(presupuestoObj){
+                const { presupuesto, restante} = presupuestoObj;
+                const restanteDiv = document.querySelector('.restante');
+            }
   }
   
 
